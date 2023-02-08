3fccea93a131f7a1984db9213dd98a6232ab53e9
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-02-08 23:44:14 +0900

    int 형 최대값,최소값

--- src/main/java/org/codej/rest/algorithm/Max3.java
+++ src/main/java/org/codej/rest/algorithm/Max3.java
@@ -21,3 +21,57 @@ public class Max3 {
         System.out.println(max);
     }
 }
+class Max4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int max = a;
+        if (max < b) max = b;
+        if (max < c) max = c;
+        if (max < d) max = d;
+        System.out.println(max);
+    }
+}
+class Min3 {
+    static int min3 (int a, int b, int c) {
+        int min = a;
+        if (min > b) min = b;
+        if (min > c) min = c;
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c = 0;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        int result = min3(a, b, c);
+        System.out.println(result);
+    }
+}
+class Min4 {
+    static int min4(int a, int b, int c , int d) {
+        int min = a;
+        if (min > b) min = b;
+        if (min > c) min = c;
+        if (min > d) min = d;
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d = 0;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        int result = min4(a, b, c, d);
+        System.out.println(result);
+
+
+    }
+}
