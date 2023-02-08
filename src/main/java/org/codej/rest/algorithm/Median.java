9c726f8213d05a012f4e277aeb2be0edda2dd5ee
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-02-08 23:44:34 +0900

    결정 트리이용해서 3가지 정수중에 중앙값 찾기

---
+++ src/main/java/org/codej/rest/algorithm/Median.java
@@ -0,0 +1,34 @@
+package org.codej.rest.algorithm;
+
+import java.util.Scanner;
+
+public class Median {
+    static int med3(int a, int b, int c) {
+        if (a >= b) {
+            if (b >= c) {
+                return b;
+            } else if (c >= a) {
+                return a;
+            } else {
+                return c;
+            }
+        } else if (a > c) {
+            return a;
+        } else if (c > b) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c = 0;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        int result = med3(a, b, c);
+        System.out.println(result);
+    }
+}
