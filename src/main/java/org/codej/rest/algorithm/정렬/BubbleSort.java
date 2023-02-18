35718320ea4374632e151a470dca7758c4ccd6c9
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-02-18 16:39:32 +0900

    버블정렬 버전1

---
+++ src/main/java/org/codej/rest/algorithm/정렬/BubbleSort.java
@@ -0,0 +1,47 @@
+package org.codej.rest.algorithm.정렬;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    /** a[idx1] 와 a[idx2]의 값을 바꿉니다. */
+    static void swap(int[] a,int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    /** 버블 정렬 */
+    static void bubbleSort(int[] arr, int n){
+        for (int i =0; i<n-1; i++){
+            for (int j =n-1; j>i; j--){
+                if (arr[j-1] > arr[j]){
+                    swap(arr,j-1,j);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random ran = new Random();
+        System.out.println("버블 정렬 V1");
+        System.out.printf("요소수 : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = ran.nextInt(30);
+        }
+
+        bubbleSort(arr,n);
+
+        System.out.println("오름차순으로 정렬되었습니다.");
+
+        System.out.println(Arrays.toString(arr));
+
+
+
+    }
+}
