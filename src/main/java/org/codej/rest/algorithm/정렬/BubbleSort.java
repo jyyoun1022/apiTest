affb61f07c0d2c49e2bd7d03fbf1dcbeef36dbaf
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-02-18 17:01:55 +0900

    버블정렬 버전2

--- src/main/java/org/codej/rest/algorithm/정렬/BubbleSort.java
+++ src/main/java/org/codej/rest/algorithm/정렬/BubbleSort.java
@@ -38,6 +38,7 @@ public class BubbleSort {
             }
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random ran = new Random();
