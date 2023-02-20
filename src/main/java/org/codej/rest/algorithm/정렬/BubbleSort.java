f7cfa4fa772482e710315f5d71120837d5940b02
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-02-20 17:44:27 +0900

    버블정렬 버전2

--- src/main/java/org/codej/rest/algorithm/정렬/BubbleSort.java
+++ src/main/java/org/codej/rest/algorithm/정렬/BubbleSort.java
@@ -39,6 +39,18 @@ public class BubbleSort {
         }
     }
 
+    static void selectionSort(int[] a,int n){
+        for (int i=0; i<n-1; i++) {
+            int min = i;
+            for(int  j = i+1; j < n; j++){
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+                swap(a,i,min);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random ran = new Random();
