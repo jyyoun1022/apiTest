a23832154827719a0fe4599e1dcd7d90d3e26e97
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-02-27 05:12:31 +0900

    @PostConstruct, @PreDestroy 사용 권고

--- src/test/java/org/codej/rest/생명주기/NetworkClient.java
+++ src/test/java/org/codej/rest/생명주기/NetworkClient.java
@@ -3,6 +3,9 @@ package org.codej.rest.생명주기;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient  {
 
     private String url;
@@ -29,12 +32,14 @@ public class NetworkClient  {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disConnect();
