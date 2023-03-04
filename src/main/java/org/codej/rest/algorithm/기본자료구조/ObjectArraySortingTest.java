8672f2cd6b513e6525e7805cd17f1345a195c442
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-03-05 02:18:11 +0900

    객체배열 정렬 Integer.compare 사용하여 단건 내림,오름 차순하는 방법, Comparator.comparing(User::getAge).thenComparing(User::getName) 사용하여 다중 조건 내림,오름 차순하는 방법

--- src/main/java/org/codej/rest/algorithm/기본자료구조/ObjectArraySortingTest.java
+++ src/main/java/org/codej/rest/algorithm/기본자료구조/ObjectArraySortingTest.java
@@ -1,10 +1,15 @@
 package org.codej.rest.algorithm.기본자료구조;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+import static java.util.Collections.reverseOrder;
 
 public class ObjectArraySortingTest {
     public static void main(String[] args) {
@@ -12,6 +17,7 @@ public class ObjectArraySortingTest {
         @NoArgsConstructor
         @AllArgsConstructor
         @ToString
+        @Getter
         class User {
             String name;
             int age;
@@ -25,9 +31,12 @@ public class ObjectArraySortingTest {
 
         };
 
-        Arrays.sort(users,(u1,u2) -> Integer.compare(u2.age,u1.age)); // 내림차순
+//        Arrays.sort(users,(u1,u2) -> Integer.compare(u2.age,u1.age)); // 내림차순
 //        Arrays.sort(users,(u1,u2) -> Integer.compare(u1.age,u2.age));// 내림차순
-
+//        Arrays.sort(users, Comparator.comparing(User::getAge));//나이순정렬
+//        Arrays.sort(users,Comparator.comparing(User::getName)); //이름순 정렬
+//        Arrays.sort(users,Comparator.comparing(User::getAge).thenComparing(User::getName));// 먼저 나이순으로 정렬하고 나이가 같다면 따로 이름순으로 정렬
+        Arrays.sort(users,Comparator.comparing(User::getAge,reverseOrder()).thenComparing(User::getName,reverseOrder()));// 먼저 나이순으로 역순정렬하고 나이가 같다면 따로 이름순으로 역순정렬
         System.out.println(Arrays.toString(users));
     }
 }
