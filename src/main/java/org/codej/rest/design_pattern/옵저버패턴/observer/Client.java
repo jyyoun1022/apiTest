a8ebfc9d1350b50b66290806b5193c439805058f
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-03-10 02:16:50 +0900

    옵저버 패턴을 사용하였을 때

--- src/main/java/org/codej/rest/design_pattern/옵저버패턴/observer/Client.java
+++ src/main/java/org/codej/rest/design_pattern/옵저버패턴/observer/Client.java
@@ -4,6 +4,7 @@ public class Client {
     public static void main(String[] args) {
 
         WeatherAPI api = new WeatherAPI();
+
         api.registerObserver(new KoreanUser("홍길동"));
         api.registerObserver(new KoreanUser("임꺽정"));
         api.registerObserver(new KoreanUser("세종대왕"));
