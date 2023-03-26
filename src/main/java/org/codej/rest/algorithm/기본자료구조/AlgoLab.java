ef5b9317a359141cca0453f435ccf584a05b2864
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-03-27 04:38:02 +0900

    String 사칙연산하기

---
+++ src/main/java/org/codej/rest/algorithm/기본자료구조/AlgoLab.java
@@ -0,0 +1,23 @@
+package org.codej.rest.algorithm.기본자료구조;
+
+import java.util.Arrays;
+
+public class AlgoLab {
+    public static void main(String[] args) {
+        String min = "1 + 24 + 3 + 40 - 21";
+        min = min.replaceAll("- ", "-").replaceAll("[+] ","").trim();
+        int sum = Arrays.stream(min.split(" ")).mapToInt(Integer::parseInt).sum();
+        System.out.println(sum);
+        System.out.println(solution("1 + 24 + 3 + 40 - 21"));
+    }
+    private static int solution(String my_string){
+
+        String[] str = my_string.split(" ");
+        int answer= Integer.parseInt(str[0]);
+
+        for (int i= 0; i< str.length; i++){
+
+        }
+        return answer;
+    }
+}
